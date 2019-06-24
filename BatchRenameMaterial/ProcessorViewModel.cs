68c4ee7720e294631dcf43346e7ba507eeca50e5
VikudouSennin <41282771+VikudouSennin@users.noreply.github.com>
2019-06-25 03:22:02 +0700

    Fix Crash, Icon Changed
    * Add Folder have some errors.

--- BatchRenameMaterial/ProcessorViewModel.cs
+++ BatchRenameMaterial/ProcessorViewModel.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
+using MaterialDesignThemes.Wpf;
+
 namespace BatchRenameMaterial
 {
     class ProcessorViewModel
     {
         public string IconKind { get; set; }
+        public string IconImg { set; get; }
         public string ProcessorName { get; set; }
         public string ToolTipsText { get; set; }
         public ICommand Commander { get; set; }
